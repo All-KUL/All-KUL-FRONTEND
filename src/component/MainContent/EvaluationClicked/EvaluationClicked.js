56f9071c3a4d79684d6c59f4fdc85bfa2965d24c
jungan777 <jungan0403@gmail.com>
2024-05-21 21:32:20 +0900

    ADD: Enrollment System

--- src/component/MainContent/EvaluationClicked/EvaluationClicked.js
+++ src/component/MainContent/EvaluationClicked/EvaluationClicked.js
@@ -1,9 +1,11 @@
 import "../../../index.css";
+import EnrollmentWebSocket from "../RegistrationClicked/Socket/EnrollmentWebSocket";
 export default function EvaluationClicked() {
     return (
         <div style={{ display: "flex",marginLeft:"312px" }}>
                 <text>미개발 페이지
                 </text>
+                <EnrollmentWebSocket />
         </div>
     );
 }
